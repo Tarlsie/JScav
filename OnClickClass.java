ee90048695b776d77567ef8f3f42475da8700af1
Tarlsie <danieltarlow@gmail.com>
2014-03-31 12:50:00 +0300

    Create OnClickClass.java

---
+++ OnClickClass.java
@@ -0,0 +1,35 @@
+package com.example.jerusalemscav;
+
+import android.view.View;
+
+public class OnClickClass implements View.OnClickListener{
+	private View view;
+	public int i;
+	
+	public void OnClick(View v, int i) {
+		this.i = i;
+		this.view = v;
+	}
+
+	
+	
+	/*@Override
+	public void onClick(View v) {
+		passButtonDetails data = new passButtonDetails();
+		inputFName = getResources().getText(fNameInput[i].getId()).toString();
+		inputLName = getResources().getText(lNameInput[i].getId()).toString();
+		toast(inputFName);
+		data.setFirstName(inputFName);
+		data.setLastname(inputLName);
+		playerDetailsButton[i].setTag(data);
+		
+		passButtonDetails buttonData = (passButtonDetails)v.getTag();
+		String fName = buttonData.getFirstName();
+		String lName = buttonData.getLastName();
+		String fullName = fName+" " +lName;
+		toast(fullName);
+		
+	}*/
+	
+
+}
